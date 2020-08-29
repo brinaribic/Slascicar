7494909dbf18aa8a5f76e8328cc3dfb6cdcce223
Brina Ribič <brinaribic19@gmail.com>
2020-08-29 11:55:59 +0200

    sprememba v imenu

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ slascicar = Slascicar()
 
 posta = slascicar.dodaj_prodajo('po posti')
 osebno = slascicar.dodaj_prodajo('osebno')
-niprodano = slascicar.dodaj_prodajo('prazno')
+niprodano = slascicar.dodaj_prodajo('neprodano')
 
 sestavine = slascicar.dodaj_strosek('sestavine', 20)
 dodatni_delavec = slascicar.dodaj_strosek('dodatni_delavec', 50)
@@ -15,6 +15,8 @@ mafini = slascicar.dodaj_sladico('mafini', date.today(), 10, sestavine, osebno, 
 makroni = slascicar.dodaj_sladico('makroni', date.today(), 10, sestavine, osebno, 20)
 
 slascicar.prodaj_sladico(torta, osebno, 3)
+
+slascicar.prodaj_sladico(torta, osebno, 2)
 
 print(slascicar.prodane_sladice())
 print(slascicar.neprodane_sladice())
